422b6cfb75845491891b1d603d84e672c965a288
Jonatan Pålsson <jonatan.p@gmail.com>
2012-11-03 13:01:41 +0100

    Removed unused layouts, and added resizing using M-z, M-a

--- xmonad.hs
+++ xmonad.hs
@@ -2,27 +2,29 @@ import XMonad
 import XMonad.Config.Xfce
 import XMonad.Config.Desktop
 import XMonad.Hooks.ManageDocks
-import XMonad.Layout.Fullscreen
-import XMonad.Layout.NoBorders
+--import XMonad.Layout.Fullscreen
+--import XMonad.Layout.NoBorders
 import XMonad.Hooks.SetWMName
- 
-myLayout = desktopLayoutModifiers $ tiledHalf ||| tiledTwoThirds ||| 
-              tiledThreeQuarters ||| Mirror tiledTwoThirds ||| Full ||| fullscreenLayout
+import XMonad.Layout.ResizableTile
+import XMonad.Util.EZConfig
+
+myLayout = desktopLayoutModifiers $ tiledHalf ||| 
+              Mirror tiledTwoThirds ||| Full 
  where  
       -- default tiling algorithm partitions the screen into two panes  
-      tiledTwoThirds = Tall nmaster delta (2/3)
-      tiledThreeQuarters = Tall nmaster delta (3/4)
-      tiledHalf = Tall nmaster delta (1/2)
-      fullscreenLayout = avoidStruts (
-        Tall 1 (3/100) (1/2)            |||
-        Mirror (Tall 1 (3/100) (1/2))   |||
-        noBorders (fullscreenFull Full))
+      tiledTwoThirds = ResizableTall nmaster delta (2/3) []
+--      tiledThreeQuarters = Tall nmaster delta (3/4)
+      tiledHalf = ResizableTall nmaster delta (1/2) []
+--      fullscreenLayout = avoidStruts (
+--        Tall 1 (3/100) (1/2)            |||
+--        Mirror (Tall 1 (3/100) (1/2))   |||
+--        noBorders (fullscreenFull Full))
    
       -- The default number of windows in the master pane  
       nmaster = 1  
    
       -- Percent of screen to increment by when resizing panes  
-      delta = 5/100
+      delta = 10/100
 
 myManageHook = composeAll . concat $
 	[
@@ -31,11 +33,17 @@ myManageHook = composeAll . concat $
 		 resource =? "xfce4-notifyd" 	--> doIgnore
 		]
 	]
-   
+
+myKeys :: [(String, X ())]
+myKeys = [
+            ("M-a", sendMessage MirrorExpand)
+          , ("M-z", sendMessage MirrorShrink)
+         ]
+
 main :: IO ()
 main = xmonad $ xfceConfig { 
 	layoutHook = myLayout,
 	modMask = mod4Mask,
 	manageHook = myManageHook <+> manageHook xfceConfig,
   startupHook = setWMName "LG3D" -- This is to convince Java that we're a legit WM.
-}
+} `additionalKeysP` myKeys
